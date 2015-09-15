68ec5832dd3e71d26727289294ee4185712bbb00
parfaittsimi <tsimiparfait@gmail.com>
2015-09-15 17:40:59 +0100

    first commit

---
+++ formulaire.php
@@ -0,0 +1,11 @@
+<p>Bonjour !</p>
+<p>Je sais comment tu t'appelles. Tu t'appelles 
+<?php echo $_POST['Nom']; ?> 
+<?php echo $_POST['Prenom']; ?>
+</p>
+<p> Tu as
+<?php echo $_POST['Age']; ?> ans
+</p>
+<p>Si tu veux changer ton identification ,
+ <a href="index.html">clique ici</a> 
+ pour revenir a la page formulaire.php.</p>
